db2909d3e8b03531c89406def5d94fe0b81327de
João Guilherme <jgwaquim@hotmail.com>
2019-09-25 13:22:01 -0300

    Update Banco.java
    Issue #21

--- Banco.java
+++ Banco.java
@@ -30,4 +30,27 @@ public class Banco {
 		return cliente;
 	}
 
+	public void selecionarOp(int op, Conta conta){
+		if(op == 1){
+			System.out.println("Sua conta eh: " + conta.getConta());
+			System.out.println("\nSua agencia eh: " + conta.getAgencia())
+		} else if(op == 2){
+
+				} else if(op == 3){
+					Operacoes op = new Operacoes();
+					System.out.println("Digite 1-SAQUE, 2-DEPOISTO, 3-TRANSFERENCIA: ");
+					Scanner ler = new Scanner(System.in);
+					n = ler.nextInt();
+					if(n==1 || n==2 || n==3){
+						op.setTipo(n);
+						op.fazerOperacao();
+					} else {
+						System.out.println("\nNumero invalido!\n");
+					}
+
+				} 
+
+
+	}
+
 }
